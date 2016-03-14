db6d10d64c9fed5103086e788212a65d210b0c09
alexey-savin <alexeyv.savin@gmail.com>
2016-03-14 19:02:51 +0300

    initial commit

---
+++ SSD.MakeImagesForStore/SSD.MakeImagesForStore/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SSD.MakeImagesForStore
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void buttonBrowseSourceImage_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker filePicker = new FileOpenPicker();
+            filePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            filePicker.ViewMode = PickerViewMode.Thumbnail;
+            filePicker.FileTypeFilter.Add(".jpg");
+            filePicker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await filePicker.PickSingleFileAsync();
+            // for multiple selection
+            //IReadOnlyList<StorageFile> fileList = await filePicker.PickMultipleFilesAsync();
+            if (file != null)
+            {
+                ViewModel.SelectedFile = file;
+            }
+        }
+
+        private async void buttonBrowseTarget_Click(object sender, RoutedEventArgs e)
+        {
+            FolderPicker folderPicker = new FolderPicker();
+            folderPicker.ViewMode = PickerViewMode.Thumbnail;
+            folderPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            folderPicker.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                ViewModel.TargetFolder = folder;
+            }
+        }
+
+        private async void buttonMakeImages_Click(object sender, RoutedEventArgs e)
+        {
+            textBlockDone.Visibility = Visibility.Collapsed;
+
+            using (var sourceStream = await ViewModel.SelectedFile.OpenReadAsync())
+            {
+                var decoder = await BitmapDecoder.CreateAsync(sourceStream);
+                
+                using (var targetStream = new InMemoryRandomAccessStream())
+                {
+                    foreach (var targetSize in ViewModel.TargetSizes)
+                    {
+                        targetStream.Size = 0;
+
+                        var encoder = await BitmapEncoder.CreateForTranscodingAsync(targetStream, decoder);
+                        encoder.BitmapTransform.ScaledHeight = targetSize.Height;
+                        encoder.BitmapTransform.ScaledWidth = targetSize.Width;
+                        await encoder.FlushAsync();
+
+                        var targetFilename = $"{ViewModel.TargetFilenameTemplate}{targetSize.Width}x{targetSize.Height}";
+                        var targetFile = await ViewModel.TargetFolder.CreateFileAsync($"{targetFilename}.png", CreationCollisionOption.ReplaceExisting);
+
+                        using (var targetFileStream = await targetFile.OpenAsync(FileAccessMode.ReadWrite))
+                        {
+                            await RandomAccessStream.CopyAndCloseAsync(targetStream.GetInputStreamAt(0), targetFileStream.GetOutputStreamAt(0));
+                        }
+                    }
+                }
+            }
+
+            textBlockDone.Visibility = Visibility.Visible;
+        }
+
+        private ViewModel ViewModel
+        {
+            get { return (ViewModel)DataContext; }
+        }
+    }
+}
---
+++ SSD.MakeImagesForStore/SSD.MakeImagesForStore/ViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SSD.MakeImagesForStore
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private StorageFile _selectedFile = null;
+        private StorageFolder _targetFolder = null;
+        private BitmapImage _image = null;
+        private string _targetFilenameTemplate = string.Empty;
+        private List<BitmapSize> _targetSizes = new List<BitmapSize>();
+
+        public ViewModel()
+        {
+            TargetFilenameTemplate = "output_";
+
+            AddTargetSquareSize(310);
+            AddTargetSquareSize(150);
+            AddTargetSquareSize(200);
+            AddTargetSquareSize(100);
+            AddTargetSquareSize(75);
+            AddTargetSquareSize(63);
+            AddTargetSquareSize(50);
+            AddTargetSquareSize(176);
+            AddTargetSquareSize(88);
+            AddTargetSquareSize(44);
+            AddTargetSquareSize(66);
+            AddTargetSquareSize(55);
+            AddTargetSquareSize(256);
+            AddTargetSquareSize(48);
+            AddTargetSquareSize(99);
+            AddTargetSquareSize(210);
+            AddTargetSquareSize(62);
+            AddTargetSquareSize(70);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string TargetFilenameTemplate
+        {
+            get { return _targetFilenameTemplate; }
+            set
+            {
+                _targetFilenameTemplate = value;
+                OnPropertyChanged("TargetFilenameTemplate");
+            }
+        }
+
+        public StorageFolder TargetFolder
+        {
+            get { return _targetFolder; }
+            set
+            {
+                if (_targetFolder == null || !_targetFolder.Equals(value.Path))
+                {
+                    _targetFolder = value;
+
+                    OnPropertyChanged("TargetFolder");
+                    OnPropertyChanged("CanMakeImages");
+                }
+            }
+        }
+
+        public StorageFile SelectedFile
+        {
+            get { return _selectedFile; }
+            set
+            {
+                if (_selectedFile == null || !_selectedFile.Name.Equals(value.Name))
+                {
+                    _selectedFile = value;
+                    CreateBitmapImage();
+
+                    OnPropertyChanged("SelectedFile");
+                    OnPropertyChanged("CanMakeImages");
+                }
+            }
+        }
+
+        public BitmapImage Image
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                OnPropertyChanged("Image");
+            }
+        }
+
+        public List<BitmapSize> TargetSizes
+        {
+            get { return _targetSizes; }
+        }
+
+        public bool CanMakeImages
+        {
+            get
+            {
+                return (_selectedFile != null && _targetFolder != null);
+            }
+        }
+
+        private async void CreateBitmapImage()
+        {
+            if (_selectedFile != null)
+            {
+                var stream = await _selectedFile.OpenReadAsync();
+                var image = new BitmapImage();
+                await image.SetSourceAsync(stream);
+
+                Image = image;
+            }
+        }
+
+        private void AddTargetSquareSize(uint size)
+        {
+            _targetSizes.Add(new BitmapSize() { Height = size, Width = size });
+        }
+    }
+}
